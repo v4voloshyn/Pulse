5d68fe9a41869199475137f96bb32c4ce8d72330
v4voloshyn <v4voloshyn@gmail.com>
2021-03-14 21:55:06 +0200

    Запилил JS на модалках-ПопАпчиках и кнопках

--- src/js/script.js
+++ src/js/script.js
@@ -35,6 +35,22 @@ $(document).ready(function(){
 		});
 	};
 
-	toggleSlide('.catalog-item__link')
-	toggleSlide('.catalog-item__link-back')
- });
+	toggleSlide('.catalog-item__link');
+	toggleSlide('.catalog-item__link-back');
+
+	//Modal
+	$('[data-modal=consultation]').on('click', function(){
+		$('.overlay, #consultation').fadeIn(500);
+	});
+
+	$('.modal__close').on('click', function(){
+		$('.overlay, #consultation, #thanks, #order').fadeOut(500);
+	})
+
+	$('.button_mini').each(function(i) {
+		$(this).on('click', function() {
+			$('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text())
+			$('.overlay, #order').fadeIn(500);
+		})
+	});
+});
