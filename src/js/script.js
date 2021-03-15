57a6a8b3366aba5d513f01afe131a75ed355486b
v4voloshyn <v4voloshyn@gmail.com>
2021-03-15 22:59:06 +0200

    Запилил отправку формы с 'локального' email на тестовый. PS: php файлы не заполнены полностью. Рабочие файлы хранятся  локально т.к. имеют конфид. информацию. Б-безопасность

--- src/js/script.js
+++ src/js/script.js
@@ -53,4 +53,21 @@ $(document).ready(function(){
 			$('.overlay, #order').fadeIn(500);
 		})
 	});
+
+	//Отправка писем с формы сайта
+	$('form').submit(function(e) {
+		e.preventDefault();
+		$.ajax({
+			type: "POST",
+			url: "mailer/smart.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			$('#consultation, #order').fadeOut();
+			$('.overlay, #thanks').fadeIn(500);
+
+			$('form').trigger('reset');
+		});
+		return false;
+	});
 });
